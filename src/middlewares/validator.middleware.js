0b6ea7848339030676ccb7df704d2f2585b96041
DavidOrtG <dortegon@unab.edu.co>
2023-10-04 14:48:21 -0500

    se agrego un validador de correo

--- src/middlewares/validator.middleware.js
+++ src/middlewares/validator.middleware.js
@@ -2,6 +2,6 @@ export const validateSchema = (schema) => (req, res, next) => {
     try {
         schema.parse(req.body);
     } catch (error) {
-        return res.status(400).json({ error: error.errors.map((error)=>error.message)});
+        return res.status(400).json(error.errors.map((error)=>error.message));
     }
 };
